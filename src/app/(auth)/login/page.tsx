d2569867a2e902541124762eab1d8941196c21e0
SAFWAT <safwathkhan2@gmail.com>
2023-08-03 03:25:25 +0530

    file setup

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,9 @@
+export default function LoginPage() {
+    return (
+        <div>
+            <h1 className="font-bold text-red-200">
+                Login Page
+            </h1>
+        </div>
+    );
+}
