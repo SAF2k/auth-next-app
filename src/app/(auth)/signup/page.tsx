a3573d9a7b42c5451aac9440892b1d0a096b32f1
SAFWAT <safwathkhan2@gmail.com>
2023-08-03 23:33:08 +0530

    working on login page

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -7,8 +7,7 @@ import React from "react";
 import { toast } from "react-hot-toast";
 
 export default function SignUpPage() {
-
-    const router = useRouter();
+  const router = useRouter();
 
   const [user, setUser] = React.useState({
     email: "",
@@ -22,28 +21,40 @@ export default function SignUpPage() {
 
   const onSignup = async () => {
     try {
-      setLoading(true)
+      setLoading(true);
 
-      const  response = await axios.post("/api/users/signup", user);
+      const response = await axios.post("/api/users/signup", user);
       router.push("/login");
-      toast.success("signup success")
-    } catch (error : any) {
-      toast.error(error.message)
-    }finally{
-        setLoading(false)
+      toast.success("signup success");
+    } catch (error: any) {
+      toast.error(error.message);
+    } finally {
+      setLoading(false);
     }
   };
 
   React.useEffect(() => {
-      if(user.email && user.password && user.username){
-          setButtonDisabled(false)
-      }else{
-          setButtonDisabled(true)
-      }
-  },[user])
+    if (user.email && user.password && user.username) {
+      setButtonDisabled(false);
+    } else {
+      setButtonDisabled(true);
+    }
+  }, [user]);
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <div
+        className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+        aria-hidden="true"
+      >
+        <div
+          className="relative left-[calc(50%-11rem)] aspect-[1055/778] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[70.1875rem]"
+          style={{
+            clipPath:
+              "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+          }}
+        />
+      </div>
       <h1>{loading ? "Processing" : "SignUp"}</h1>
       <hr />
       <label htmlFor="username">username</label>
@@ -77,8 +88,22 @@ export default function SignUpPage() {
         onClick={onSignup}
         className="p-2 mt-4 text-white bg-blue-500 rounded-lg disabled:opacity-50"
         disabled={buttonDisabled}
-      >signup</button>
+      >
+        signup
+      </button>
       <Link href="/login">All ready a member</Link>
+      <div
+        className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
+        aria-hidden="true"
+      >
+        <div
+          className="relative left-[calc(50%+3rem)] aspect-[1455/538] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#f02e65] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
+          style={{
+            clipPath:
+              "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+          }}
+        />
+      </div>
     </div>
   );
 }
