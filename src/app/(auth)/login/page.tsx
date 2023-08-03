82dba8df5a425940f2afbbdef148d1cf1b3fde80
SAFWAT <safwathkhan2@gmail.com>
2023-08-03 18:31:39 +0530

    signup page added

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,9 +1,45 @@
+"use client";
+
+import Link from "next/link";
+import React, { useState } from "react";
+
 export default function LoginPage() {
-    return (
-        <div>
-            <h1 className="font-bold text-red-200">
-                Login Page
-            </h1>
-        </div>
-    );
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+
+  const onLogin = async () => {};
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1>Login</h1>
+      <hr />
+      <label htmlFor="email">email</label>
+      <input
+        className="p-2 text-black rounded-lg"
+        type="email"
+        id="email"
+        value={user.email}
+        onChange={(e) => setUser({ ...user, email: e.target.value })}
+        placeholder="email"
+      />
+      <label htmlFor="password">password</label>
+      <input
+        className="p-2 text-black rounded-lg"
+        type="password"
+        id="password"
+        value={user.password}
+        onChange={(e) => setUser({ ...user, password: e.target.value })}
+        placeholder="password"
+      />
+      <button
+        onClick={onLogin}
+        className="p-2 mt-4 text-white bg-blue-500 rounded-lg"
+      >
+        Login
+      </button>
+      <Link href="/signup">not a member?</Link>
+    </div>
+  );
 }
--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,9 +1,84 @@
+"use client";
+
+import axios from "axios";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import React from "react";
+import { toast } from "react-hot-toast";
+
 export default function SignUpPage() {
-    return (
-        <div>
-            <h1>
-                SignUpPage
-            </h1>
-        </div>
-    );
+
+    const router = useRouter();
+
+  const [user, setUser] = React.useState({
+    email: "",
+    password: "",
+    username: "",
+  });
+
+  const [buttonDisabled, setButtonDisabled] = React.useState(false);
+
+  const [loading, setLoading] = React.useState(false);
+
+  const onSignup = async () => {
+    try {
+      setLoading(true)
+
+      const  response = await axios.post("/api/users/signup", user);
+      router.push("/login");
+      toast.success("signup success")
+    } catch (error : any) {
+      toast.error(error.message)
+    }finally{
+        setLoading(false)
+    }
+  };
+
+  React.useEffect(() => {
+      if(user.email && user.password && user.username){
+          setButtonDisabled(false)
+      }else{
+          setButtonDisabled(true)
+      }
+  },[user])
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1>{loading ? "Processing" : "SignUp"}</h1>
+      <hr />
+      <label htmlFor="username">username</label>
+      <input
+        className="p-2 text-black rounded-lg"
+        type="text"
+        id="username"
+        value={user.username}
+        onChange={(e) => setUser({ ...user, username: e.target.value })}
+        placeholder="username"
+      />
+      <label htmlFor="email">email</label>
+      <input
+        className="p-2 text-black rounded-lg"
+        type="email"
+        id="email"
+        value={user.email}
+        onChange={(e) => setUser({ ...user, email: e.target.value })}
+        placeholder="email"
+      />
+      <label htmlFor="password">password</label>
+      <input
+        className="p-2 text-black rounded-lg"
+        type="password"
+        id="password"
+        value={user.password}
+        onChange={(e) => setUser({ ...user, password: e.target.value })}
+        placeholder="password"
+      />
+      <button
+        onClick={onSignup}
+        className="p-2 mt-4 text-white bg-blue-500 rounded-lg disabled:opacity-50"
+        disabled={buttonDisabled}
+      >signup</button>
+      <Link href="/login">All ready a member</Link>
+    </div>
+  );
 }
