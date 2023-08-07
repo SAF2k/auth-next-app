38b2e098be7d45d1d9f346165223cc503bb9976d
SAFWAT <safwathkhan2@gmail.com>
2023-08-07 23:16:51 +0530

    fixeded mailer bug

--- src/app/api/users/verifyemail/route.ts
+++ src/app/api/users/verifyemail/route.ts
@@ -1,12 +1,39 @@
 import { connect } from "@/database/dbconfig";
-import { NextRequest } from "next/server";
+import User from "@/models/userModel";
+import { NextRequest, NextResponse } from "next/server";
 
-connect()
+connect();
 
 export async function POST(request: NextRequest) {
-    try {
+  try {
+      
+      const reqBody = await request.json();
+      const { token } = reqBody;
+      console.log(token);
+      const user = await User.findOne({
+          verifyToken: token,
+          verifyTokenExpiry: { $gt: Date.now() },
+        });
         
-    } catch (error: any) {
-        
+        console.log("mailer");
+    if (!user) {
+      return NextResponse.json(
+        { error: "Invalid token or token expired" },
+        { status: 400 }
+      );
     }
+    console.log(user);
+
+    user.isVerified = true;
+    user.verifyToken = undefined;
+    user.verifyTokenExpiry = undefined;
+    await user.save();
+
+    return NextResponse.json({
+      message: "Email verified successfully",
+      success: true,
+    });
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
 }
