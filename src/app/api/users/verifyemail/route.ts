514b34e3cddf9884b642c2caf46cdf29359a7ec5
SAFWAT <safwathkhan2@gmail.com>
2023-08-07 22:48:02 +0530

    mailer completed

---
+++ src/app/api/users/verifyemail/route.ts
@@ -0,0 +1,12 @@
+import { connect } from "@/database/dbconfig";
+import { NextRequest } from "next/server";
+
+connect()
+
+export async function POST(request: NextRequest) {
+    try {
+        
+    } catch (error: any) {
+        
+    }
+}
