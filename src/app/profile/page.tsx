6715e8b3c08e0a490bce1f561d4a609623ca30de
SAFWAT <safwathkhan2@gmail.com>
2023-08-07 22:05:45 +0530

    added middleware & fetch userid

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,12 +1,14 @@
-'use client'
+"use client";
 
 import axios from "axios";
+import Link from "next/link";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 import { toast } from "react-hot-toast";
 
 export default function ProfilePage() {
-
   const router = useRouter();
+  const [data, setData] = useState("nothing");
 
   const onLogout = async () => {
     try {
@@ -16,14 +18,38 @@ export default function ProfilePage() {
     } catch (error: any) {
       toast.error(error.message);
     }
-  }
+  };
 
-    return (
-      <div className="flex flex-col items-center justify-center min-h-screen py-2">
-        <h1>Profile</h1>
-        <hr />
-        <p>Profile Page</p>
-        <button onClick={onLogout} className="border rounded-md px-4 py-2 mt-5 hover:bg-red-600 hover:text-black">LOGOUT</button>
-      </div>
-    );
+  const getUserDetails = async () => {
+    const response = await axios.get("/api/users/user");
+    console.log(response.data);
+    setData(response.data.data._id);
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1>Profile</h1>
+      <hr />
+      <p>Profile Page</p>
+      <h2>
+        {data === "nothing" ? (
+          "Nothing"
+        ) : (
+          <Link href={`/profile/${data}`}>{data}</Link>
+        )}
+      </h2>
+      <button
+        onClick={onLogout}
+        className="border rounded-md px-4 py-2 mt-5 hover:bg-red-600 hover:text-black"
+      >
+        LOGOUT
+      </button>
+      <button
+        onClick={getUserDetails}
+        className="text-center border rounded-md px-4 py-2 mt-5 hover:bg-red-600 hover:text-black"
+      >
+        Get User Details
+      </button>
+    </div>
+  );
 }
