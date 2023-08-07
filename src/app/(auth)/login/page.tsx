ac507e5923be4ed5ed64800a5d62f19878ff1cc1
SAFWAT <safwathkhan2@gmail.com>
2023-08-07 05:34:06 +0530

    added profile

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -22,12 +22,11 @@ export default function LoginPage() {
     try {
       setLoading(true);
       const response = await axios.post("/api/users/login", user);
-      console.log(response.data);
       toast.success(response.data.message);
       router.push("/profile");
     } catch (error: any) {
       toast.error(error.message);
-      
+      console.log("hello");   
     } finally {
       setLoading(false);
     }
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,14 +1,29 @@
+'use client'
+
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import { toast } from "react-hot-toast";
+
 export default function ProfilePage() {
+
+  const router = useRouter();
+
+  const onLogout = async () => {
+    try {
+      const response = await axios.get("/api/users/logout");
+      toast.success(response.data.message);
+      router.push("/login");
+    } catch (error: any) {
+      toast.error(error.message);
+    }
+  }
 
     return (
       <div className="flex flex-col items-center justify-center min-h-screen py-2">
         <h1>Profile</h1>
-        <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-          hello
-        </div>
-        <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-2/3 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-         dsadsda
-        </div>
+        <hr />
+        <p>Profile Page</p>
+        <button onClick={onLogout} className="border rounded-md px-4 py-2 mt-5 hover:bg-red-600 hover:text-black">LOGOUT</button>
       </div>
     );
 }
