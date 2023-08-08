8da6fd96c3ad8ecf0dd3001c5fa467aa51da7dc7
SAFWAT <safwathkhan2@gmail.com>
2023-08-08 22:35:30 +0530

    added verify email

--- src/app/api/users/verifyemail/route.ts
+++ src/app/api/users/verifyemail/route.ts
@@ -1,26 +1,22 @@
-import { connect } from "@/database/dbconfig";
-import User from "@/models/userModel";
 import { NextRequest, NextResponse } from "next/server";
+import User from "@/models/userModel";
+import { connect } from "@/database/dbconfig";
 
 connect();
 
 export async function POST(request: NextRequest) {
   try {
-      
-      const reqBody = await request.json();
-      const { token } = reqBody;
-      console.log(token);
-      const user = await User.findOne({
-          verifyToken: token,
-          verifyTokenExpiry: { $gt: Date.now() },
-        });
-        
-        console.log("mailer");
+    const reqBody = await request.json();
+    const { token } = reqBody;
+    console.log(token);
+
+    const user = await User.findOne({
+      verifyToken: token,
+      verifyTokenExpiry: { $gt: Date.now() },
+    });
+
     if (!user) {
-      return NextResponse.json(
-        { error: "Invalid token or token expired" },
-        { status: 400 }
-      );
+      return NextResponse.json({ error: "Invalid token" }, { status: 400 });
     }
     console.log(user);
 
--- src/helpers/mailer.ts
+++ src/helpers/mailer.ts
@@ -33,13 +33,11 @@ export const sendEmail = async ({ email, emailType, userId }: any) => {
       to: email,
       subject:
         emailType === "VERIFY" ? "Verify your email" : "Reset your password",
-      html: `<p>Click <a href="${
-        process.env.DOMAIN
-      }/verifyemail?token=${hashedToken}">here</a> to ${
+      html: `<p>Click <a href="${process.env.domain}/verifyemail?token=${hashedToken}">here</a> to ${
         emailType === "VERIFY" ? "verify your email" : "reset your password"
       }
             or copy and paste the link below in your browser. <br> ${
-              process.env.DOMAIN
+              process.env.domain
             }/verifyemail?token=${hashedToken}
             </p>`,
     };
